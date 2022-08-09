4eb2d5d2dd7c415595f31a9a4cdc35c17c358df4
jflaher4 <jflaher4@nd.edu>
2022-08-09 09:20:33 -0400

    Changed navigation

---
+++ index.js
@@ -0,0 +1,11 @@
+function scrollToPartners() {
+  document.getElementById('partners').scrollIntoView({behavior: 'smooth'});
+}
+
+function scrollToStaff() {
+  document.getElementById('staff').scrollIntoView({behavior: 'smooth'});
+}
+
+function scrollToJoinUs() {
+  document.getElementById('join-us').scrollIntoView({behavior: 'smooth'});
+}
