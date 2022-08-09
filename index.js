e4e6e75ec4b5e21217b4e779d218f71a7de4d77b
jflaher4 <jflaher4@nd.edu>
2022-08-09 15:56:26 -0400

    Added some animations

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 function scrollToHome() {
   document.getElementById('home').scrollIntoView({behavior: 'smooth'});
+  document.getElementById('mission').classList.add('visible');
 }
 
-function scrollToPartners() {
+async function scrollToPartners() {
+  document.getElementById('mission').classList.remove('visible');
+  var partnerCards = document.getElementsByClassName('partner-card');
+  for (var i = 0; i < partnerCards.length; i++) {
+    partnerCards[i].classList.remove('visible');
+  }
   document.getElementById('partners').scrollIntoView({behavior: 'smooth'});
+  for (var i = 0; i < partnerCards.length; i++) {
+    var partnerCard = partnerCards[i];
+    partnerCard.classList.add('visible');
+    await delay(350);
+  }
 }
 
-function scrollToStaff() {
+async function scrollToStaff() {
+  document.getElementById('mission').classList.remove('visible');
+  var staffRows = document.querySelectorAll('tr');
+  for (var i = 0; i < staffRows.length; i++) {
+    staffRows[i].classList.remove('visible');
+  }
   document.getElementById('staff').scrollIntoView({behavior: 'smooth'});
+  for (var i = 0; i < staffRows.length; i++) {
+    staffRows[i].classList.add('visible');
+    await delay(75);
+  }
 }
 
 function scrollToJoinUs() {
+  document.getElementById('mission').classList.remove('visible');
   document.getElementById('join-us').scrollIntoView({behavior: 'smooth'});
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
 }
