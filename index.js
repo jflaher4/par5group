d17177e644ea359250ccd9c03729a7faceeb8184
jflaher4 <jflaher4@nd.edu>
2023-09-13 16:55:59 -0500

    Changed animation mechanics

--- index.js
+++ index.js
@@ -2,28 +2,41 @@ function scrollToHome() {
   document.getElementById('home').scrollIntoView({behavior: 'smooth'});
 }
 
+
+var firstPartnerScroll = false;
 async function scrollToPartners() {
-  var partnerCards = document.getElementsByClassName('partner-card');
-  for (var i = 0; i < partnerCards.length; i++) {
-    partnerCards[i].classList.remove('visible');
-  }
-  document.getElementById('partners').scrollIntoView({behavior: 'smooth'});
-  for (var i = 0; i < partnerCards.length; i++) {
-    var partnerCard = partnerCards[i];
-    partnerCard.classList.add('visible');
-    await delay(350);
+  if (!firstPartnerScroll) {
+    firstPartnerScroll = true;
+    var partnerCards = document.getElementsByClassName('partner-card');
+    for (var i = 0; i < partnerCards.length; i++) {
+      partnerCards[i].classList.remove('visible');
+    }
+    document.getElementById('partners').scrollIntoView({behavior: 'smooth'});
+    for (var i = 0; i < partnerCards.length; i++) {
+      var partnerCard = partnerCards[i];
+      partnerCard.classList.add('visible');
+      await delay(350);
+    }
+  } else {
+    document.getElementById('partners').scrollIntoView({behavior: 'smooth'});
   }
 }
 
+var firstStaffScroll = false;
 async function scrollToStaff() {
-  var staffRows = document.getElementsByClassName('tr');
-  for (var i = 0; i < staffRows.length; i++) {
-    staffRows[i].classList.remove('visible');
-  }
-  document.getElementById('staff').scrollIntoView({behavior: 'smooth'});
-  for (var i = 0; i < staffRows.length; i++) {
-    staffRows[i].classList.add('visible');
-    await delay(75);
+  if (!firstStaffScroll) {
+    firstStaffScroll = true;
+    var staffRows = document.getElementsByClassName('tr');
+    for (var i = 0; i < staffRows.length; i++) {
+      staffRows[i].classList.remove('visible');
+    }
+    document.getElementById('staff').scrollIntoView({behavior: 'smooth'});
+    for (var i = 0; i < staffRows.length; i++) {
+      staffRows[i].classList.add('visible');
+      await delay(75);
+    }
+  } else {
+    document.getElementById('staff').scrollIntoView({behavior: 'smooth'});
   }
 }
 
